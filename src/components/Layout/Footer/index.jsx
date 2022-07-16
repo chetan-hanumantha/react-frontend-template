18045683f3f8d1f5fbfa89f1d475674c39d9d5ba
dreamcatcher0101 <dreamcatcher000101@gmail.com>
2022-07-16 11:09:16 -0400

    Update footer

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FooterSection = () => {
   return (
     <footer className="flex justify-center p-4 text-gray-100 bg-gray-800 w-full fixed bottom-0">
-      <h1 className="text-xl">Designed by Steven K. Fish</h1>
+      <h1 className="text-xl">Designed by Dream Catcher</h1>
     </footer>
   );
 };
